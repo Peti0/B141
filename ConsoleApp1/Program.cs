19ed5717d9a44f693e630a17f7fa55e11eeb49b3
Peti0 <szegedipeti2003@gmail.com>
2023-09-12 08:54:40 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // - két szám bekérése és összeadása
+            int szamA;
+            int szamB;
+            Console.WriteLine("Adja meg az első számot!");
+            szamA = int.Parse(Console.ReadLine());
+            Console.WriteLine("Adja meg a második számot!");
+            szamB = int.Parse(Console.ReadLine());
+            Console.WriteLine("A két szám összege:" + (szamA+szamB).ToString());
+            
+            Console.ReadLine();
+        }
+        
+    }
+}
